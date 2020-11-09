378715e334eb7113a7d415349fa67232315173ca
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-09 07:29:09 +0000

    Deploy yudbox/mern-links to github.com/yudbox/mern-links.git:gh-pages

---
+++ precache-manifest.93f60301309aa2727d0dfd5fac8c85c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0841d9a60c374b50bbaf1095f343c59d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f10a737a2e4836c2f53e",
+    "url": "./static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "e585ab49ed5580644177",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "f10a737a2e4836c2f53e",
+    "url": "./static/js/2.861303f0.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "./static/js/2.861303f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e585ab49ed5580644177",
+    "url": "./static/js/main.e19d0cb0.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
